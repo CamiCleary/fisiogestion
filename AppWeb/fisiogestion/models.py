594718b3265810dedea6f9e7e1e2b2010e7d6dde
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2025-06-16 01:21:32 -0400

    Se agregaron los nuevos modelos

--- AppWeb/fisiogestion/models.py
+++ AppWeb/fisiogestion/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+class Administrador(models.Model):
+    email = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=128)  # Almacenar contraseña de forma segura
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
 class Paciente(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     cedula = models.CharField(max_length=20, unique=True)
     telefono = models.CharField(max_length=20)
     email = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=128)
     direccion = models.CharField(max_length=255)
 
     def __str__(self):
@@ -15,6 +22,7 @@ class Fisioterapeuta(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     email = models.EmailField()
+    contraseña = models.CharField(max_length=128)  # Almacenar contraseña de forma segura
     telefono = models.CharField(max_length=20)
     rol = models.CharField(max_length=50)
     especialidad = models.CharField(max_length=100)
